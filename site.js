2ce4ba9b912c57f56f25fd51d6377aa633b5a80a
Advait Joshi <ajoshi18@hawk.iit.edu>
2018-11-23 13:56:06 -0600

    Fixed link issues in href

--- site.js
+++ site.js
@@ -1,21 +1,40 @@
-(function(){
-
+ /* (function(){
   // Check to make sure essential features are supported
   if (typeof document.querySelector === 'undefined') {
-  // if not, bye!
-  return;
+    // No? Then bye!!!
+    return;
   }
 
-  document.addEventListener('DOMContentLoaded', function(){};
-    // Diagnostic...make sure the DOMContentLoaded has fired...
-    // Grab the HTML Element
-      var html_tag = document.querySelector('html');
-      
-    console.log('DOM has loaded successfully.')
-    // Go and remove the .nojs class from html
-    // .. and then add a .js class
-    html_tag.className= 'js';
-    // if yes, continue
+  document.addEventListener('DOMContentLoaded', function(){
+    // Diagnostic...make sure DOMContentLoaded has fired...
+    console.log('OMG the DOM has loaded!');
+
+    // Grab the HTML element:
+    var html_tag = document.querySelector('html');
+
+    // Go and remove the .nojs class from <html>...
+    html_tag.classList.remove('nojs');
+
+    // And then add the .js class to <html>:
+    html_tag.classList.add('js');
+    var heading_label = 'Menu';
+    var heading = document.querySelector('links');
+    heading.innerHTML = '<a href="#links">'+heading_label+'</a>';
+    heading.addEventListener('click',function(){
+      var content = document.querySelector('#links');
+      var label = document.querySelector('links a');
+
+      content.classList.toggle('visible');
+
+      if (label.textContent === heading_label) {
+        label.textContent = 'Click to Hide Content';
+      } else {
+        label.textContent = heading_label;
+      }
     })
-  console.log("Hello from Javascript! You have querySelector!");
+  });
+
+  console.log("Hello from JavaScript! You have querySelector!");
 })();
+
+/*
